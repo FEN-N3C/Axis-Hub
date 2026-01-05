a38c010db6597434308d13813edba134b5c6596f
FEN_N3C <117320391+FEN-N3C@users.noreply.github.com>
2026-01-05 18:21:43 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,10 +1,14 @@
 local Config = {
-	Enabled = false,
-	TeamCheck = true,
-	DeadCheck = true,
-	WallCheck = true,
-	Prediction = 0.13,
-	LockType = "ClosestToCursor"
+    Enabled = false,
+    TeamCheck = true,
+    DeadCheck = true,
+    WallCheck = true,
+    Prediction = 0.13,
+    LockType = "ClosestToCursor",
+    
+    LockPart = "HumanoidRootPart",
+    RandomizePart = false,
+    RandomizeInterval = 1
 }
 
 return Config
