ae901ce7978075242e76ecba650cd4e620d86001
FEN_N3C <117320391+FEN-N3C@users.noreply.github.com>
2026-01-08 20:59:13 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,5 @@
 local Config = {
+    -- AIMBOT
     Enabled = false,
     TeamCheck = false,
     DeadCheck = false,
@@ -6,10 +7,18 @@ local Config = {
     FriendCheck = false,
     Prediction = 0,
     LockType = "ClosestToCursor",
-    
     LockPart = "HumanoidRootPart",
     RandomizePart = false,
-    RandomizeInterval = 1
-}
+    RandomizeInterval = 1,
+
+    -- FOV
+    FOVEnabled = false,
+    FOVVisible = false,
+    FOVRadius = 70,
+    FOVThickness = 2,
+    FOVOpacity = 0,
+    FOVColor = Color3.fromRGB(255, 255, 255)
+
+    }
 
 return Config
